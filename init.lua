aa0384fb2c25709d83078e0a2b414989b397b11a
Daniel Toint <Gedztodu@gmail.com>
2023-04-18 17:03:52 +0300

    Add macos binds

--- init.lua
+++ init.lua
@@ -2,5 +2,13 @@ require('base')
 require('highlights')
 require('keymaps')
 
+local has = function(x)
+  return vim.fn.has(x) == 1
+end
 
+local is_mac = has "macunix"
 
+if is_mac then
+  print('start macos mode')
+  require('macos')
+end
